6ee792c6c4328f29adc1be887d8f90c5b7671d7c
Víctor <victor97pe@gmail.com>
2018-06-02 11:31:04 +0200

    Países y provincias

---
+++ provincias.sql
@@ -0,0 +1,117 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-06-2018 a las 11:27:07
+-- Versión del servidor: 10.1.31-MariaDB
+-- Versión de PHP: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `introjobs`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `provincias`
+--
+
+CREATE TABLE `provincias` (
+  `id` int(11) NOT NULL,
+  `provincia` varchar(80) COLLATE utf8_spanish_ci DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 CHECKSUM=1 COLLATE=utf8_spanish_ci DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC;
+
+--
+-- Volcado de datos para la tabla `provincias`
+--
+
+INSERT INTO `provincias` (`id`, `provincia`) VALUES
+(1, 'Álava'),
+(2, 'Albacete'),
+(3, 'Alicante'),
+(4, 'Almería'),
+(5, 'Ávila'),
+(6, 'Badajoz'),
+(7, 'Illes Balears'),
+(8, 'Barcelona'),
+(9, 'Burgos'),
+(10, 'Cáceres'),
+(11, 'Cádiz'),
+(12, 'Castellón'),
+(13, 'Ciudad Real'),
+(14, 'Córdoba'),
+(15, 'A Coruña'),
+(16, 'Cuenca'),
+(17, 'Girona'),
+(18, 'Granada'),
+(19, 'Guadalajara'),
+(20, 'Guipúzcoa'),
+(21, 'Huelva'),
+(22, 'Huesca'),
+(23, 'Jaén'),
+(24, 'León'),
+(25, 'Lleida'),
+(26, 'La Rioja'),
+(27, 'Lugo'),
+(28, 'Madrid'),
+(29, 'Málaga'),
+(30, 'Murcia'),
+(31, 'Navarra'),
+(32, 'Ourense'),
+(33, 'Asturias'),
+(34, 'Palencia'),
+(35, 'Las Palmas'),
+(36, 'Pontevedra'),
+(37, 'Salamanca'),
+(38, 'Santa Cruz de Tenerife'),
+(39, 'Cantabria'),
+(40, 'Segovia'),
+(41, 'Sevilla'),
+(42, 'Soria'),
+(43, 'Tarragona'),
+(44, 'Teruel'),
+(45, 'Toledo'),
+(46, 'Valencia'),
+(47, 'Valladolid'),
+(48, 'Vizcaya'),
+(49, 'Zamora'),
+(50, 'Zaragoza'),
+(51, 'Ceuta'),
+(52, 'Melilla');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `provincias`
+--
+ALTER TABLE `provincias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `provincias`
+--
+ALTER TABLE `provincias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
